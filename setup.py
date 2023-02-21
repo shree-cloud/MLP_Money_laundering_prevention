6911bf19a4e9eb736dc751c3612e9d7f22eea5d0
shree <you@example.com>
2023-02-21 20:49:47 +0530

    project structure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from typing import List
 REQUIREMENT_FILE_NAME = "requirements.txt"
 HYPHEN_E_DOT = "-e ."
 
-def get_requirements()->list[str]:
+def get_requirements()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
     requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
