bda3b83ada11465367e7590036c3ceda77d45059
shree <shreyashmohod@gmail.com>
2023-02-24 15:06:50 +0530

    logger and exception

--- main.py
+++ main.py
@@ -2,19 +2,9 @@ from elleptic.logger import logging
 from elleptic.exception import ElepticException
 import os,sys
 
-def test():
-     try:
-          logging.info("start test log")
-          result = 3/0
-          print(result)
-          logging.info("stop test log")
-
-     except Exception as e:
-          logging.debug("str")
-          raise ElepticException(e, sys)
 
 if __name__=="__main__":
      try:
-          test()
+          pass
      except Exception as e:
           print(e)
